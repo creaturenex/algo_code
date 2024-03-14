c19cb6fe282f606c147f6355da60fbd30066e678
Oscar Romero <creaturenex@gmail.com>
2024-03-13 20:29:56 -0400

    initial commit

---
+++ problems/uncompress.js
@@ -0,0 +1,16 @@
+// Multiline comment
+/*
+
+*/
+
+// Closure compiler - JSDOC
+/**
+ * 
+ * @param {string} string
+ * @returns {string} 
+ */
+
+const uncompress = (s) => {
+  let result = ''
+  return result
+};
